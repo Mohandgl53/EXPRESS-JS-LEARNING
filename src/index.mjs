bdf0085260245cce053e4baa2bcf0074b46e5c96
Mohan Kumar <tomhollderdgl53@gmail.com>
2026-02-25 23:10:27 +0530

    Learned to connect using Mongooose

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,16 @@ import cookieParser from 'cookie-parser';
 import session from "express-session";
 import { Strategy as LocalStrategy } from "passport-local";
 import passport from 'passport';
-import { users } from './utils/constants.mjs';
 import mongoose from "mongoose";
+import { User } from './mongoose/schema/user.mjs';
 
 const app = express()
 app.use(express.json());
 app.use(cookieParser("HEllo"));
 
 mongoose.connect('mongodb://localhost/express_course')
-.then(()=>console.log("MongoDb Connected "))
-.catch((err)=>console.log(`Error: ${err}`));
+    .then(() => console.log("MongoDb Connected "))
+    .catch((err) => console.log(`Error: ${err}`));
 
 app.use(
     session({
@@ -28,15 +28,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(
     { usernameField: "user_name", passwordField: "password" },
-    (user_name, password, done) => {
-        const user = users.find(user => user.user_name == user_name);
-        if (!user) {
-            return done(null, false, { message: "Invalid Username" });
+    async (user_name, password, done) => {
+        try {
+            const user = await User.findOne({ user_name: user_name });
+            if (!user) {
+                return done(null, false, { message: "Invalid Username" });
+            }
+            if (user.password != password) {
+                return done(null, false, { message: "Incorrect Password" });
+            }
+            return done(null, user);
         }
-        if (user.password != password) {
-            return done(null, false, { message: "Incorrect Password" });
+        catch (err) {
+            console.log(err);
+            return done(err, false);
         }
-        return done(null, user);
+
+
     }
 ));
 
@@ -45,8 +53,14 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    const user = users.find((u) => u.id === id);
-    done(null, user || false);
+    try {
+        const user = User.findById(id);
+        done(null, user || false);
+    } catch (err) {
+        console.log(err);
+        done(err, false);
+    }
+
 })
 
 app.use(Router);
